b4a406d6fb46e0831f65977787fc6a87990d90dc
nz816 <nz816@nyu.edu>
2020-11-24 20:50:25 -0500

    jquery

---
+++ peer_programming/script.js
@@ -0,0 +1,103 @@
+// TITLES, DEVELOPER, SONG NAME, COMPOSER, ALBUM
+
+let metadata = [
+  ["Sly 2: Band of Thieves", "Sucker Punch", "Paris(Sly in Paris)", "Peter McConnell", "Sly 2: Band of Thieves Soundtrack"],
+  ["Minecraft", "Mojang Studios", "Dry Hands", "Daniel Rosenfeld (C418)", "Minecraft - Volume Alpha"],
+  ["Bugsnax", "Young Horses", "Sunrise over Snaxtooth Island", "Seth Parker", "Bugsnax - Original Soundtrack"],
+  ["Journey", "thatgamecompany", "I Was Born for This", "Austin Winsory", "Journey"],
+  ["Celeste", "Matt Makes Games", "First Steps", "Lena Raine", "Celeste Original Soundtrack"],
+  ["Crash Bandicoot", "Naughty Dog", "Map", "Josh Mancell", "Crash Bandicoot Soundtrack"],
+  ["The Last of Us", "Naughty Dog", "The Last of Us", "Gustavo Santaolalla", "The Last of Us"],
+  ["Nier: Automata", "PlatinumGames", "City Ruins (Shade)", "Keiichi Okabe", "NieR: Automata Original Soundtrack"],
+  ["Wii Sports", "Nintendo EAD", "Title Screen", "Kazumi Totaka", "Wii Sports Original Soundtrack"],
+  ["Undertale", "Toby Fox", "Snowdin Town", "Toby Fox", "UNDERTALE Soundtrack"],
+  ["Kingdom Hearts 2", "Square Enix", "The 13th Reflection", "Yoko Shimomura", "KINGDOM HEARTS Original Soundtrack COMPLETE"]];
+
+let descriptions = [
+  ""
+]
+
+
+  $("#sly2").click(function() {
+    console.log("sly2 clicked");
+    $(".box-art").css("width", "40px");
+    $("#sly2").css('width', "210px");
+  }
+  );
+
+  $("#minecraft").click(function() {
+    console.log("minecraft clicked");
+    $(".box-art").css("width", "40px");
+    $("#minecraft").css('width', "210px");
+  }
+  );
+
+  $("#bugsnax").click(function() {
+    console.log("bugsnax clicked");
+    $(".box-art").css("width", "40px");
+    $("#bugsnax").css('width', "230px");
+  }
+  );
+
+  $("#journey").click(function() {
+    console.log("journey clicked");
+    $(".box-art").css("width", "40px");
+    $("#journey").css('width', "260px");
+  }
+  );
+
+  $("#celeste").click(function() {
+    console.log("celeste clicked");
+    $(".box-art").css("width", "40px");
+    $("#celeste").css('width', "170px");
+  }
+  );
+
+  $("#crashbandicoot").click(function() {
+    console.log("crashbandicoot clicked");
+    $(".box-art").css("width", "40px");
+    $("#crashbandicoot").css('width', "300px");
+  }
+  );
+
+  $("#lastofus").click(function() {
+    console.log("lastofus clicked");
+    $(".box-art").css("width", "40px");
+    $("#lastofus").css('width', "250px");
+  }
+  );
+
+  $("#nier").click(function() {
+    console.log("nier clicked");
+    $(".box-art").css("width", "40px");
+    $("#nier").css('width', "240px");
+  }
+  );
+
+  $("#kingdomhearts").click(function() {
+    console.log("kingdomhearts clicked");
+    $(".box-art").css("width", "40px");
+    $("#kingdomhearts").css('width', "210px");
+  }
+  );
+
+  $("#wiisports").click(function() {
+    console.log("wiisports clicked");
+    $(".box-art").css("width", "40px");
+    $("#wiisports").css('width', "350px");
+  }
+  );
+
+  $("#undertale").click(function() {
+    console.log("undertale clicked");
+    $(".box-art").css("width", "40px");
+    $("#undertale").css('width', "300px");
+  }
+  );
+
+  $("#terraria").click(function() {
+    console.log("terraria clicked");
+    $(".box-art").css("width", "40px");
+    $("#terraria").css('width', "210px");
+  }
+  );
