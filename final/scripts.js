271453757bd48e4d664d5cd78e8676729d3be988
nz816 <nz816@nyu.edu>
2020-12-08 20:38:49 -0500

    finished final

--- final/scripts.js
+++ final/scripts.js
@@ -9,13 +9,20 @@ $(".btn").click(function(){
     console.log("button clicked");
     count += 1;
     console.log("Number of inputs: " + count);
+
+    new Audio("sparkle.mp3").play();
+
     $("#" + count).text($("#affirmationInput").val());
 
     $("#flower-image").attr("src","image" + String(count+1) + ".png")
 
+    $("#flower" + String(count)).show("slow");
+
     if(count == 6){
-        $('.message').show();
+        $('.message').show(4000);
+        $('.footer').show();
         $('.submission-container').remove();
+        $('.expand-one').text("🌼");
     } 
 }
 );
