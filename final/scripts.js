0e301a776c7c5b8ce3e8659d174fc0bd964763fa
nz816 <nz816@nyu.edu>
2020-12-12 13:12:55 -0500

    added web responsiveness

--- final/scripts.js
+++ final/scripts.js
@@ -1,23 +1,26 @@
 var count = 0;
 
+// click flower to reveal submission box
 $('.expand-one').click(function(){
     $('.submission').slideToggle('slow');
 }
 );
 
+// submission things
 $(".btn").click(function(){
     console.log("button clicked");
+    //increment nuber of affirmations
     count += 1;
     console.log("Number of inputs: " + count);
-
+    //sparkle sound plays
     new Audio("sparkle.mp3").play();
-
+    //changes text of affirmation to user input
     $("#" + count).text($("#affirmationInput").val());
-
+    //updates flower stage
     $("#flower-image").attr("src","image" + String(count+1) + ".png")
-
+    //reveals rotating flower next to affirmation
     $("#flower" + String(count)).show("slow");
-
+    //things that happen once all affirmations are entered
     if(count == 6){
         $('.message').show(4000);
         $('.footer').show();
